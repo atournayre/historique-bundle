3123949b4f99645d253107fe6b2578aaed5087f2
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2022-10-14 15:31:02 +0200

    Update Configuration->getConfigTreeBuilder().

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('atournayre');
+        $treeBuilder = new TreeBuilder('atournayre');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
